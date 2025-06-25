d93e300ec4d7f785141f2528b6508ac0f84ad16b
Enercido Alcantara <enercido.alcantara@voxmedia.com>
2025-06-25 18:21:29 -0400

    passing the options object

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import compileStyles from './src/styles/index.js';
 import compileScripts from "./src/scripts/config.js";
 
 // todo: capture and pass down the compiler options, e.g. watch, minify, dev, prod, etc.
+const options = {};
 
-await compileMedia()
-await compileStyles()
-await compileScripts()
+await compileMedia(options)
+await compileStyles(options)
+await compileScripts(options)
